22ddbf96730cad0b6453ba3b41afef799a52723d
kevinhsu1987 <50226558+kevinhsu1987@users.noreply.github.com>
2020-01-04 21:05:01 +0800

    Delete DBSource.java

--- src/DBSource.java
+++
@@ -1,7 +0,0 @@
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface DBSource {
-    public Connection getConnection() throws SQLException;
-    public void closeConnection(Connection conn) throws SQLException;
-}
